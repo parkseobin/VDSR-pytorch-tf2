b3952d905fe504973f17c0e351be78910a41bda5
seobin <kor.p.sb@gmail.com>
2020-04-06 18:16:19 +0900

    .

--- pytorch/dataset.py
+++ pytorch/dataset.py
@@ -46,37 +46,9 @@ class mydata(Dataset):
 
         return img_item
     
-    
-class testOnly_data(Dataset):
-    def __init__(self, LR_path, in_memory = True, transform = None):
-        
-        self.LR_path = LR_path
-        self.LR_img = sorted(os.listdir(LR_path))
-        
-        if in_memory:
-            self.LR_img = [np.array(Image.open(os.path.join(self.LR_path, lr))) for lr in self.LR_img]
-        
-    def __len__(self):
-        
-        return len(self.LR_img)
-        
-    def __getitem__(self, i):
-        
-        img_item = {}
-        
-        if in_memory:
-            LR = self.LR_img[i]
-            
-        else:
-            LR = np.array(Image.open(os.path.join(self.LR_path, self.LR_img[i])))
-
-        img_item['LR'] = (LR / 127.5) - 1.0                
-        img_item['LR'] = img_item['LR'].transpose(2, 0, 1).astype(np.float32)
-        
-        return img_item
 
 
-class crop(object):
+ class crop(object):
     def __init__(self, scale, patch_size):
         
         self.scale = scale
